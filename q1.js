b3c3639a0834a20f9d765cec2e2333dd39ca8b12
DWAUUD BELKO <belkodawuud@gmail.com>
2022-03-15 17:04:06 +0100

    q1

---
+++ q1.js
@@ -0,0 +1,9 @@
+//myFunction is the name of the function.
+//num1 and num2 are the parameters of the function.
+//what myFunction is doing is add num1 to num2
+//The value for result is 10 becouse 5+5 is 10.
+
+function myFunction(num1, num2) {
+  console.log(num1 + num2);
+}
+const result = myFunction(5, 5);
