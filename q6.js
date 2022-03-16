499a2ca370cfc055408b4da4f4b72e42f4c52f65
DWAUUD BELKO <belkodawuud@gmail.com>
2022-03-16 11:26:46 +0100

    q6

---
+++ q6.js
@@ -0,0 +1,16 @@
+//The function has two parameters num and num1.
+//The value that will be logged inside this function will be 2
+//Because we looged num1 and num1 is given a value of 2.
+//And aslo it is used when invoking the function.
+//If we had logged both num1 and num e.g.console,log(num1,num)
+//Then we will have both 2 and 10 because that are the values each is asigned to.
+//And both were used when invoking the function.
+
+function myFunction(num, num1) {
+  console, log(num1);
+}
+
+let num = 10;
+let num1 = 2;
+
+myFunction(num1, num);
