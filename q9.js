f5b72a9f7c38cb6882a6a70946f47833f03a5b5d
DWAUUD BELKO <belkodawuud@gmail.com>
2022-03-17 10:27:46 +0100

    q9

---
+++ q9.js
@@ -0,0 +1,12 @@
+//20 will be logged because the values of the arguments given before the function was invoked.
+//If the funtion was invoked before the values where given it will log nudefined.
+
+function myFunction(num1, num2) {
+  console.log(num3);
+}
+
+const num1 = 10;
+const num2 = 1;
+const num3 = 20;
+
+myFunction(num3, num1);
